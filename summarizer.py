f231c94842383647752e2d6f6930b146fc14c7b0
Allu Pooja Sathvika <allupooja09@gmail.com>
2025-06-22 10:15:40 +0530

    file changed

--- summarizer.py
+++ summarizer.py
@@ -1,5 +1,5 @@
 import nltk
-nltk.data.path.append("nltk_data")
+nltk.download("punkt")
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
