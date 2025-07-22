6b2249f781b90150e1d7c7d37fd7374226842586
Allu Pooja Sathvika <allupooja09@gmail.com>
2025-07-22 16:55:23 +0530

    summarizer updated

--- summarizer.py
+++ summarizer.py
@@ -1,11 +1,21 @@
 import nltk
+import spacy
 nltk.download("punkt")
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 
+nlp=spacy.load("en_core_web_sm")
+
+def spacy_sent_tokenize(text):
+    doc=nlp(text)
+    return [sent.text.strip() for sent in doc.sents]
+
 def summarize_text(text,num_sentences=5):
-    parser=PlaintextParser.from_string(text,Tokenizer("english"))
+    sentences=spacy_sent_tokenize(text)
+    clean_text=" ".join(sentences)
+
+    parser=PlaintextParser.from_string(clean_text,Tokenizer("english"))
     summarizer=LexRankSummarizer()
     summary=summarizer(parser.document,num_sentences)
     return "\n\n".join(f"• {sentence}" for sentence in summary)
