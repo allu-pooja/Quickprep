1c177b4a24540794ac6287cc7ad8151d9f7d2126
Allu Pooja Sathvika <allupooja09@gmail.com>
2025-07-22 22:33:47 +0530

    file updated

--- summarizer.py
+++ summarizer.py
@@ -1,12 +1,20 @@
 import nltk
+import os
 import spacy
-nltk.data.path.append('nltk_data')
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 
 import spacy.cli
 
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+nltk.data.path.append(nltk_data_path)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
+    
 try:
     nlp = spacy.load("en_core_web_sm")
 except OSError:
