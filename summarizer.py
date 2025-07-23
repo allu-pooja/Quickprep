f2de17877d00e93bf0ff50e42b4afb3839b21bc3
Allu Pooja Sathvika <allupooja09@gmail.com>
2025-07-23 09:56:28 +0530

    summarizer file updated

--- summarizer.py
+++ summarizer.py
@@ -2,18 +2,18 @@ import nltk
 import os
 import spacy
 from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
-
+import pickle
 import spacy.cli
+from nltk.tokenize.punkt import PunktSentenceTokenizer
 
-nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
-nltk.data.path.append(nltk_data_path)
+nltk_path = os.path.join(os.path.dirname(__file__), "nltk_data")
+punkt_path = os.path.join(nltk_path, "tokenizers", "punkt", "english.pickle")
+nltk.data.path.append(nltk_path)
 
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt', download_dir=nltk_data_path)
+with open(punkt_path, "rb") as f:
+    punkt_tokenizer = PunktSentenceTokenizer(pickle.load(f))
+
     
 try:
     nlp = spacy.load("en_core_web_sm")
@@ -25,11 +25,18 @@ def spacy_sent_tokenize(text):
     doc=nlp(text)
     return [sent.text.strip() for sent in doc.sents]
 
+# Custom tokenizer using Punkt
+class CustomSumyTokenizer:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+    def to_sentences(self, text):
+        return self.tokenizer.tokenize(text)
+
 def summarize_text(text,num_sentences=5):
     sentences=spacy_sent_tokenize(text)
     clean_text=" ".join(sentences)
 
-    parser=PlaintextParser.from_string(clean_text,Tokenizer("english"))
+    parser = PlaintextParser.from_string(clean_text, CustomSumyTokenizer(punkt_tokenizer))
     summarizer=LexRankSummarizer()
     summary=summarizer(parser.document,num_sentences)
     return "\n\n".join(f"• {sentence}" for sentence in summary)
